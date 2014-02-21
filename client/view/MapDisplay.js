c8e540d4fbf908faa98deebed83f02b3c0861bb7
Bharat Ponnaluri <bponnaluri@gmail.com>
2014-02-20 22:11:19 -0600

    Fixed territory shapes

--- client/view/MapDisplay.js
+++ client/view/MapDisplay.js
@@ -45,22 +45,22 @@ function MapDisplay(topX,topY,mapImg,dataCon,titleView,scoreView,background,pNam
 
         Object.keys(data).forEach(function(reg){
             var rData=data[reg]
-            d3.select("svg").selectAll("path#"+reg)
-                .attr("transform","translate("+rData["x"]+","+rData["y"]+")scale(0.25)")
+
             console.log("text#"+reg)
-            d3.select("svg").selectAll("text#"+reg)
+
+            var svg=d3.select("svg")
+            svg.selectAll("text#"+reg)
                 .attr("x",rData["x"])
                 .attr("y",rData["y"])
                 .attr("fill","black")
+                .attr("transform","translate(10,20)")
+
+            svg.selectAll("path#"+reg)
+                .attr("transform","scale(0.25)")
 
         });
 
-        var svg=d3.select("svg")
-        console.log(svg.selectAll("path#Scotland"))
 
-        var rData=data["Scotland"]
-        d3.select("svg").selectAll("path#Scotland")
-            .attr("transform","translate("+(rData["x"]-20)+","+(rData["y"]-50)+")scale(0.25)")
     }
 
 
@@ -88,30 +88,24 @@ function MapDisplay(topX,topY,mapImg,dataCon,titleView,scoreView,background,pNam
             return;
         }
 
-        g.drawImage(background,0,0);//Load a background.
         titleView.drawSelf();
         scoreView.drawSelf();
-        g.drawImage(mapImg,topX,topY,mapImg.width/4,mapImg.height/4);
+
 
         gameState["regionStates"].map(function(state){
+
 
             d3.select("svg")
                 .selectAll("path#"+state["name"])
                 .attr("fill",getColor(state["owner"]))
+
+
             d3.select("svg")
                 .selectAll("text#"+state["name"])
                 .text(state["army"])
 
 
-            g.fillStyle=getColor(state["owner"]);
-            g.font='10pt Calibri';
-            g.fillText(""+state["army"],transX(state["xPos"])+10,transY(state["yPos"])+20);
 
-            g.fillStyle='black';
-            var sX=transX(state["xPos"]);
-            var sY=transY(state["yPos"]);
-            var width=Math.floor(30.0*state["hitPoints"]/1000.0);
-            g.fillRect(sX,sY,width,10);
         });
 
         gameState["moveCommands"][pName].forEach(function(state){
@@ -119,6 +113,7 @@ function MapDisplay(topX,topY,mapImg,dataCon,titleView,scoreView,background,pNam
             g.moveTo(transX(state["x1"]+10),transY(state["y1"])+10);
             g.lineTo(transX(state["x2"]+10),transY(state["y2"])+10);
             g.stroke();
+
         });
         //Indicate that an area has been clicked.
         var clickData=dataCon.getSavedClick();
@@ -130,7 +125,7 @@ function MapDisplay(topX,topY,mapImg,dataCon,titleView,scoreView,background,pNam
         g.fillText("Click",transX(x),transY(y));
 
         var end=new Date().getTime();
-        //console.log("Time "+(end-start));
+      //  console.log("Time "+(end-start));
 
     }
 }
